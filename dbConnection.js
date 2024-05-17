e05a428e2a6d211bc17f9940dc3bb116e4b43942
@notlim <124739255+omosh-notlim@users.noreply.github.com>
2024-05-17 09:17:08 +0300

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 // Load environment variables from .env file
 dotenv.config();
 
-const sequelize = new Sequelize({
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-});
+const sequelize = new Sequelize(
+    process.env.PG_DB,
+    process.env.PG_USER,
+    process.env.PG_PASSWORD,
+    {
+        host: process.env.PG_HOST,
+        dialect: 'postgres',
+    }
+);
 
 export default sequelize;
