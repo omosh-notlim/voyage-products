82db5e9695de2d9b3cb5730bac69ed6d02f52c3f
Milton Omondi <klsictintern1@siginon.com>
2024-05-20 21:24:08 +0300

    green voyage products

--- dbConnection.js
+++ dbConnection.js
@@ -1,8 +1,4 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-dotenv.config();
 
 const sequelize = new Sequelize({
     dialect: process.env.DB_DIALECT,
