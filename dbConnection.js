eef4d7bbcd9da2a23be8bcb7cca6f2663cb6e106
Milton Omondi <klsictintern1@siginon.com>
2024-05-20 22:16:10 +0300

    green voyage products

--- dbConnection.js
+++ dbConnection.js
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-    process.env.DB_PORT,
     process.env.DB_DATABASE,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD,
     {
         dialect: process.env.DB_DIALECT,
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
     }
 );
 
